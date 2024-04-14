281630fea00671aeae02e84f6a0341e1ecab335b
Zhoucai <nizhoucai@gmail.com>
2024-04-14 03:20:45 -0400

    add password

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Home } from "./pages/Home";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Lock from "./pages/Lock";
 // import { Login } from "./pages/Login";
 // import Usernav from "./componenets/Usernav";
 // import { AuthContext } from './context/AuthContext';
@@ -15,8 +16,8 @@ function App() {
       {/* {currentUser ? <Usernav /> : <Header />} */}
       <Routes>
         <Route path="/">
-          <Route index element={<Home />} />
-          {/* <Route path="login" element={<Login />} /> */}
+          <Route index element={<Lock />} />
+          <Route path="home" element={<Home />} />
           {/* <Route path="signup" element={<Register />} /> */}
           {/* <Route path="profile/:id" element={<Profile />} /> */}
         </Route>
