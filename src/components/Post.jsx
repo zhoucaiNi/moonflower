61c9aa2a8cf60d24903bfae3705916558f9a3939
Zhoucai <nizhoucai@gmail.com>
2024-04-14 03:42:57 -0400

    added markdown

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Draggable from 'react-draggable';
 import ReactMarkdown from 'react-markdown'
+import "../styles/note.scss"
 
 const Post = (props) => {
 
   return (
-    <div className="post">
-      <Draggable
+    <div >
+      <Draggable className="note"
       // handle=".move-icon" // this is for you to define, what part of the note do you want to drag by
       // grid={[5, 5]} // snapping to grid pixels
       // defaultPosition={{ x: 0, y: 0 }} // if no position given
@@ -18,8 +19,9 @@ const Post = (props) => {
       // onStop={this.handleStopDrag}
       >
         <div>
-          <h1 >{props.post.content || ""} </h1>
-          {/* <ReactMarkdown> {props.post.content || ""} </ReactMarkdown> */}
+          {/* <h1 >{props.post.content || ""} </h1> */}
+          <ReactMarkdown>{props.post.content || ""}</ReactMarkdown>
+
         </div>
       </Draggable>
     </div>
