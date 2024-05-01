c0880e3e2ef4200c7489233f885798137974eecf
Zhoucai <nizhoucai@gmail.com>
2024-05-01 16:59:46 -0400

    bg

--- src/pages/Lock.jsx
+++ src/pages/Lock.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import "../styles/lock.scss"
-import { Container, Text, Input, Button, InputGroup, Flex, Center } from '@chakra-ui/react';
+import { Container, Text, Input, Button, InputGroup, Flex, Center, Box } from '@chakra-ui/react';
 
 
 const Lock = () => {
@@ -16,17 +16,27 @@ const Lock = () => {
   }
   return (
     <>
-      <Flex width={"100vw"} height={"100vh"} alignContent={"center"} justifyContent={"center"}>
-        <Center>
-          <Container>
-            <Text>Please enter the password </Text>
-            <InputGroup size='md'>
-              <Input type="text" onChange={(e) => setPassword(e.target.value)} />
-              <Button type="button" value="submit" onClick={() => handleLock()} > Unlock </Button>
-            </InputGroup>
-          </Container>
-        </Center>
-      </Flex>
+
+      <Box
+        bgImage="url('https://upload.wikimedia.org/wikipedia/commons/e/e2/Animated_y2k_nightclub_floor.gif')"
+        bgPosition="center"
+        bgRepeat="no-repeat"
+      >
+
+        <Flex width={"100vw"} height={"100vh"} alignContent={"center"} justifyContent={"center"}>
+          <Center>
+            <Box bg="white" p="4" borderRadius="md" boxShadow="md">
+              <Container>
+                <Text>Please enter the password </Text>
+                <InputGroup size='md'>
+                  <Input mr="10px" type="text" onChange={(e) => setPassword(e.target.value)} />
+                  <Button type="button" value="submit" onClick={() => handleLock()} > Unlock </Button>
+                </InputGroup>
+              </Container>
+            </Box>
+          </Center>
+        </Flex>
+      </Box>
     </>
   )
 }
